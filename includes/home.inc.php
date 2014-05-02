a17be091f16b0b84f54ff7c7ee2ca404e39c63ac
ramosjrz <jrzr94@gmail.com>
2014-05-02 12:35:43 +0800

    Updated homepage

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,13 +8,13 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <title>PLDT EGMM Homepage</title>
+    <title>PLDT | East GMM CoreNet Portal</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/dashboard.css" rel="stylesheet">
+    <link href="dashboard.css" rel="stylesheet">
 
     <link href="css/main.css" rel="stylesheet">
 
@@ -40,7 +39,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="#"><img src="assets/pldt logo.png" height="60px"/></a>
+          <a href=""><img src="pldt logo.png" height="60px"/></a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
@@ -51,7 +50,6 @@
           </ul>
          <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Search...">
-<<<<<<< HEAD
           </form> 
         </div>
       </div>
@@ -62,50 +60,173 @@
 				<div class="col-sm-3 col-md-2 sidebar">
 					<div class="well">
 					<ul id="sidenav" class="nav nav-pills nav-stacked">
-						<li class="active"><a href="#tabs-basic" data-toggle="tab"><strong>Tabs</strong></a></li>
-						<li><a href="#tabs-side" data-toggle="tab"><strong>Side Tabs</strong></a></li>
-						<li><a href="#tabs-stacked" data-toggle="tab"><strong>Stacked Tabs</strong></a></li>
-						<li><a href="#pills-basic" data-toggle="tab"><strong>Pills</strong></a></li>
-						<li><a href="#pills-stacked" data-toggle="tab"><strong>Stacked Pills</strong></a></li>
+						<li class="active"><a href="#home" data-toggle="tab"><strong>Reports</strong></a></li>
+						<li><a href="#d1" data-toggle="tab"><strong>Maintenance</strong></a></li>
+						<li><a href="#d2" data-toggle="tab"><strong>Export</strong></a></li>
+            <li><a href="#d3" data-toggle="tab"><strong>Users</strong></a></li>
 					</ul>
 					</div><!-- .well -->
 				</div> <!-- col-sm -->
 			</div> <!-- sidebar -->
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <div class="tab-content">
+            <div class="tab-pane fade in active" id="home">
+              <h1>Dashboard</h1>
+              <div class="table-responsive">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                  <th>Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>Lorem</td>
+                  <td>ipsum</td>
+                  <td>dolor</td>
+                  <td>sit</td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>amet</td>
+                  <td>consectetur</td>
+                  <td>adipiscing</td>
+                  <td>elit</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>Integer</td>
+                  <td>nec</td>
+                  <td>odio</td>
+                  <td>Praesent</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>libero</td>
+                  <td>Sed</td>
+                  <td>cursus</td>
+                  <td>ante</td>
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>dapibus</td>
+                  <td>diam</td>
+                  <td>Sed</td>
+                  <td>nisi</td>
+                </tr>
+                <tr>
+                  <td>1,005</td>
+                  <td>Nulla</td>
+                  <td>quis</td>
+                  <td>sem</td>
+                  <td>at</td>
+                </tr>
+                <tr>
+                  <td>1,006</td>
+                  <td>nibh</td>
+                  <td>elementum</td>
+                  <td>imperdiet</td>
+                  <td>Duis</td>
+                </tr>
+                <tr>
+                  <td>1,007</td>
+                  <td>sagittis</td>
+                  <td>ipsum</td>
+                  <td>Praesent</td>
+                  <td>mauris</td>
+                </tr>
+                <tr>
+                  <td>1,008</td>
+                  <td>Fusce</td>
+                  <td>nec</td>
+                  <td>tellus</td>
+                  <td>sed</td>
+                </tr>
+                <tr>
+                  <td>1,009</td>
+                  <td>augue</td>
+                  <td>semper</td>
+                  <td>porta</td>
+                  <td>Mauris</td>
+                </tr>
+                <tr>
+                  <td>1,010</td>
+                  <td>massa</td>
+                  <td>Vestibulum</td>
+                  <td>lacinia</td>
+                  <td>arcu</td>
+                </tr>
+                <tr>
+                  <td>1,011</td>
+                  <td>eget</td>
+                  <td>nulla</td>
+                  <td>Class</td>
+                  <td>aptent</td>
+                </tr>
+                <tr>
+                  <td>1,012</td>
+                  <td>taciti</td>
+                  <td>sociosqu</td>
+                  <td>ad</td>
+                  <td>litora</td>
+                </tr>
+                <tr>
+                  <td>1,013</td>
+                  <td>torquent</td>
+                  <td>per</td>
+                  <td>conubia</td>
+                  <td>nostra</td>
+                </tr>
+                <tr>
+                  <td>1,014</td>
+                  <td>per</td>
+                  <td>inceptos</td>
+                  <td>himenaeos</td>
+                  <td>Curabitur</td>
+                </tr>
+                <tr>
+                  <td>1,015</td>
+                  <td>sodales</td>
+                  <td>ligula</td>
+                  <td>in</td>
+                  <td>libero</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+                  
+                </tr>
+              </table>
+            </div>
+            <div class="tab-pane fade" id="d1">
+              <h1>Dashboard 1</h1>
+            </div>
+            <div class="tab-pane fade" id="d2">
+              <h1>Dashboard 2</h1>
+            </div>
+            <div class="tab-pane" id="d3">
+              <h1>Users Management</h1>
+            </div>
+          </div>
+      </div>
 	</div> <!-- row -->
 					
-	<!-- tabs -->
 	
 	 <!-- Bootstrap core JavaScript
-=======
-          </form>
-        </div>
-      </div>
-    </div>
-  </header>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview</a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Equipment</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Equipment Map</a></li>
-          </ul>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1>Dashboard</h1>
-        </div>
-      </div>
-    </div>
-    <!-- Bootstrap core JavaScript
->>>>>>> 814c6b9453e566c1ae72fee2d5058689c19ae549
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/docs.min.js"></script>
+    <script type="text/javascript">
+      $(function () {
+        $('#sidenav a:first').tab('show')
+      })
+    </script>
   </body>
 </html>
