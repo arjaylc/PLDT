6d4caf6fcba0d79797071978e865c84041b78576
nemeCIS06 <francis_gaddi@dlsu.ph>
2014-05-06 15:31:06 +0800

    Fixed Add Node and Cabinet

--- includes/addcabinet.inc.php
+++ includes/addcabinet.inc.php
@@ -7,9 +7,20 @@
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="cabinetType" class="col-sm-2 control-label">Cabinet Type</label>
+			<label for="cabinettype" class="col-sm-2 control-label">Cabinet Type</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="sinNumber" placeholder="Enter Cabinet Type">
+				<select>
+					<?php
+				        require_once('includes/database_master.inc.php');
+				        $database_master = new DatabaseMaster();
+						$query = "SELECT cabinet_types FROM cabinet_types";
+						$queryResult = $database_master->querySelect($query);
+						foreach($queryResult as $row){?>
+							<option><?php echo $row['cabinet_types']?></option>
+					<?php
+						}
+					?>
+				</select>
 			</div>
 		</div>
 
--- includes/addnode.inc.php
+++ includes/addnode.inc.php
@@ -7,6 +7,23 @@
 			</div>
 		</div>
 		<div class="form-group">
+			<label for="cabinetnumber" class="col-sm-2 control-label">Cabinet Number</label>
+			<div class="col-sm-10">
+				<select>
+					<?php
+				        require_once('includes/database_master.inc.php');
+				        $database_master = new DatabaseMaster();
+						$query = "SELECT cabinetNo FROM cabinet";
+						$queryResult = $database_master->querySelect($query);
+						foreach($queryResult as $row){?>
+							<option><?php echo $row['cabinetNo']?></option>
+					<?php
+						}
+					?>
+				</select>
+			</div>
+		</div>
+		<div class="form-group">
 			<label for="sinNumber" class="col-sm-2 control-label">Sin Number</label>
 			<div class="col-sm-10">
 				<input type="text" class="form-control" id="sinNumber" placeholder="Enter Sin Number">
@@ -25,18 +42,20 @@
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="office" class="col-sm-2 control-label">Central Office</label>
+			<label for="office" class="col-sm-2 control-label">Office</label>
 			<div class="col-sm-10">
-				<div class="btn-group">
-					<button data-toggle="dropdown" class="btn dropdown-toggle">Office <span class="caret"></span></button>
-					<ul class="dropdown-menu">
-						<li><a href="#">NPG</a></li>
-						<li><a href="#">MYG</a></li>
-						<li><a href="#">DSL</a></li>
-						<li><a href="#">GNT</a></li>
-						<li><a href="#">PSG</a></li>
-					</ul>
-				</div>
+				<select>
+					<?php
+				        require_once('includes/database_master.inc.php');
+				        $database_master = new DatabaseMaster();
+						$query = "SELECT central_officeName FROM central_offices";
+						$queryResult = $database_master->querySelect($query);
+						foreach($queryResult as $row){?>
+							<option><?php echo $row['central_officeName']?></option>
+					<?php
+						}
+					?>
+				</select>
 			</div>
 		</div>
 		<div class="form-group">
