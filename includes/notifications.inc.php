016d27ce762b5e4873673f1d8a4b2174c6fc392b
riasan <riasantiago08@yahoo.com>
2014-05-14 09:44:21 +0800

    Notifications GUI

---
+++ includes/notifications.inc.php
@@ -0,0 +1,83 @@
+<div class="table-responsive" id="pending tasks">
+  <br>
+  <h2> Pending Tasks </h2>
+  <div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading">Pending Tasks</div>
+    <!-- Table -->
+    <table class="table" id="pending tasks">
+	  <thead>
+		<tr>
+			<th>Project Title</th>
+			<th>Node Name</th>
+			<th>Deadline</th>
+			<th>Type of Maintenance</th>
+			<th>Date Started</th>
+			<th>Date Accomplished</th>
+		</tr>
+	  </thead>
+	  <tbody>
+		<tr>
+			<td>one </td>
+			<td> two </td>
+			<td> three </td>
+			<td> four </td>
+			<td> five </td>
+			<td> six </td>
+		</tr>
+	  </tbody>
+	 </table>
+	</div> <!-- table -->
+	</div> <!-- panel -->
+	<div class="table-responsive" id="ongoing tasks">
+  <br>
+  <h2> Ongoing Tasks </h2>
+  <div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading">Ongoing Tasks</div>
+    <!-- Table -->
+    <table class="table" id="ongoing tasks">
+	  <thead>
+		<tr>
+			<th>Project Title</th>
+			<th>Node Name</th>
+			<th>Deadline</th>
+			<th>Type of Maintenance</th>
+			<th>Date Started</th>
+			<th>Date Accomplished</th>
+		</tr>
+	  </thead>
+	  <tbody>
+		<tr>
+			<td>one </td>
+			<td> two </td>
+			<td> three </td>
+			<td> four </td>
+			<td> five </td>
+			<td> six </td>
+		</tr>
+	  </tbody>
+	 </table>
+	</div> <!-- table -->
+	</div> <!-- panel -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
