7414c0aee6fe4d0986295e6a7309283cb0440bcf
ramosjrz <jrzr94@gmail.com>
2014-05-15 15:22:26 +0800

    updated look of add project

--- includes/addproject.inc.php
+++ includes/addproject.inc.php
@@ -1,3 +1,10 @@
+<?php
+$msie = strpos($_SERVER["HTTP_USER_AGENT"], 'MSIE') ? true : false;
+$firefox = strpos($_SERVER["HTTP_USER_AGENT"], 'Firefox') ? true : false;
+$safari = strpos($_SERVER["HTTP_USER_AGENT"], 'Safari') ? true : false;
+$chrome = strpos($_SERVER["HTTP_USER_AGENT"], 'Chrome') ? true : false;
+?>
+
 <button class="btn btn-success btn-lg" data-toggle="modal" data-target="#addProjectModal">Add Project</button>
 
 <!-- Add Project Modal -->
@@ -10,76 +17,115 @@
 	    </div>
 	    <div class="modal-body">
 	    	<div id="register-overlay" class = "form">
-	    		<form class="form-horizontal" role="form" action="check_project.php" method="POST"> <!--- -->
+	    		<form class="form-horizontal" role="form" action="checkAddNode.php" method="POST"> <!--- -->
 	    			<div class="form-group">
 	    				<label for="projectTitle" class="col-sm-4 control-label">Project Title</label>
 	    				<div class="col-sm-7">
-	    					<input type="text" class="form-control" id="projectTitle" placeholder="Enter Project Title" name="title">
+	    					<input type="text" class="form-control" id="projectTitle" placeholder="Enter Project Title" name="projectTitle">
 	    				</div>
 	    			</div>
 					<div class="form-group">
-						<label for="employee" class="col-sm-4 control-label">Employee Assigned</label>
+						<label for="idnumber" class="col-sm-4 control-label">ID Number</label>
 						<div class="col-sm-7">
-							<input type="text" class="form-control" id="employee" placeholder="Enter Employee Details" name="idNo">
+							<input type="text" class="form-control" id="idnumber" placeholder="Enter ID number" name="idnumber">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="nodename" class="col-sm-4 control-label">Node Name</label>
+						<div class="col-sm-7">
+							<input type="text" class="form-control" id="nodename" placeholder="Enter Node Name" name="nodename">
 						</div>
 					</div>
 
+					<!-- checklist -->
+					<?php
+						if($chrome||$safari){ 
+					?>
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label">Deadline</label>
+								<div class="col-sm-7">
+									<input type="datetime-local" class="form-control" id="deadline" placeholder="Enter Deadline Date Time" name="deadline">
+								</div>
+							</div>
+					<?php	
+					} else{
+					?>
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label">Deadline</label>
+								<div class="row">
+									<div class="col-sm-2">
+										<select class="form-control" id="dlMonth">
+											<option>mm</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dlDay">
+											<option>dd</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<input type="text" class="form-control" id="dlYear" placeholder="yyyy" maxlength="4">
+									</div>
+								</div>
+							</div>	
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label"></label>
+								<div class="row">
+									<div class="row">
+									<div class="col-sm-2">
+										<select class="form-control" id="dlhour">
+											<option>hh</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dlmins">
+											<option>mm</option>
+											<?php
+												for($i=1; $i<=59; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dlTime">
+											<option>AM</option>
+											<option>PM</option>
+										</select>
+									</div>
+								</div>
+								</div>
+							</div>
+					<?php
+					}
+					?>
+						
 					<div class="form-group">
-						<label for="projectFor" class="col-sm-4 control-label">Node or Cabinet</label>
+						<label for="typeofmaintenance" class="col-sm-4 control-label">Type of Maintenance</label>
 						<div class="col-sm-3">
-							<select id = "projectFor" name="projectFor" class="form-control"> <!-- SHALL BE EDITED -->
-								<option onclick="disableInputs()">Select...</option>
-								<option onclick="enableNodeInput()">Node</option>
-								<option onclick="enableCabinetInput()">Cabinet</option>
-							</select>
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label for="projectNodeName" class="col-sm-4 control-label">Node Name</label>
-						<div class="col-sm-7">
-							<input type="text" class="form-control" id="projectNodeName" disabled name="nodeName">
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label for="projectCabinetNo" class="col-sm-4 control-label">Cabinet Number</label>
-						<div class="col-sm-7">
-							<input type="text" class="form-control" id="projectCabinetNo" disabled name="cabinetNo">
-						</div>
-					</div>
-
-						<!-- checklist -->
-
-
-					
-					<div class="form-group">
-						<label for="projectDeadlineDate" class="col-sm-4 control-label">Deadline Date</label>
-						<div class="col-sm-7">
-							<input type="date" class="form-control" id="projectDeadlineDate" 
-							placeholder="Enter Deadline Date" name="deadlineDate">
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label for="projectDeadlineTime" class="col-sm-4 control-label">Deadline Time (optional)</label>
-						<div class="col-sm-7">
-							<input type="time" class="form-control" id="projectDeadlineTime" 
-							placeholder="Enter Deadline Time" name="deadlineTimme">
-						</div>
-					</div>
-
-					<div class="form-group">
-						<label for="projectType" class="col-sm-4 control-label">Type of Maintenance</label>
-						<div class="col-sm-3">
-							<select id = "projectType" name="projectType" class="form-control"> <!-- SHALL BE EDITED -->
+							<select id = "typeofmaintenance" name="typeofmaintenance" class="form-control"> <!-- SHALL BE EDITED -->
 								<?php
 							        require_once('includes/database_master.inc.php');
 							        $database_master = new DatabaseMaster();
-									$query = "SELECT project_type FROM project_types";
+									$query = "SELECT central_officeName FROM central_offices";
 									$queryResult = $database_master->querySelect($query);
 									foreach($queryResult as $row){?>
-										<option><?php echo $row['project_type']?></option>
+										<option><?php echo $row['central_officeName']?></option>
 								<?php
 									}
 								?>
@@ -87,17 +133,166 @@
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="faultDescription" class="col-sm-4 control-label">Fault Description</label>
+						<label for="actiontaken" class="col-sm-4 control-label">Action taken</label>
 						<div class="col-sm-7">
-							<textarea class="form-control" rows="3" id="faultDescription" placeholder="Enter Description" name="faultDescription"></textarea>
+							<textarea class="form-control" rows="3" id="actiontaken" placeholder="Enter Action Taken" name="actiontaken"></textarea>
 						</div>
 					</div>
-					<div class="form-group">
-						<label for="projectTasks" class="col-sm-4 control-label">Required Tasks</label>
-						<div class="col-sm-7">
-							<textarea class="form-control" rows="3" id="projectTasks" placeholder="Enter Tasks" name="tasks"></textarea>
-						</div>
-					</div>
+
+					<?php
+						if($chrome||$safari){ 
+					?>
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label">Date Time Started</label>
+								<div class="col-sm-7">
+									<input type="datetime-local" class="form-control" id="deadline" placeholder="Enter Date Time Started" name="dts">
+								</div>
+							</div>
+					<?php	
+					} else{
+					?>
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label">Date Time Started</label>
+								<div class="row">
+									<div class="col-sm-2">
+										<select class="form-control" id="dtsMonth">
+											<option>mm</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dtsDay">
+											<option>dd</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<input type="text" class="form-control" id="dtsYear" placeholder="yyyy" maxlength="4">
+									</div>
+								</div>
+							</div>	
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label"></label>
+								<div class="row">
+									<div class="row">
+									<div class="col-sm-2">
+										<select class="form-control" id="dtshour">
+											<option>hh</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dtsmins">
+											<option>mm</option>
+											<?php
+												for($i=1; $i<=59; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dtsTime">
+											<option>AM</option>
+											<option>PM</option>
+										</select>
+									</div>
+								</div>
+								</div>
+							</div>
+					<?php
+					}
+					?>
+
+					<?php
+						if($chrome||$safari){ 
+					?>
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label">Date Time Accomplised</label>
+								<div class="col-sm-7">
+									<input type="datetime-local" class="form-control" id="deadline" placeholder="Enter Date Time Accomplished" name="dta">
+								</div>
+							</div>
+					<?php	
+					} else{
+					?>
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label">Date Time Accomplished</label>
+								<div class="row">
+									<div class="col-sm-2">
+										<select class="form-control" id="dtaMonth">
+											<option>mm</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dtaDay">
+											<option>dd</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<input type="text" class="form-control" id="dtaYear" placeholder="yyyy" maxlength="4">
+									</div>
+								</div>
+							</div>	
+							<div class="form-group">
+								<label for="deadline" class="col-sm-4 control-label"></label>
+								<div class="row">
+									<div class="row">
+									<div class="col-sm-2">
+										<select class="form-control" id="dtahour">
+											<option>hh</option>
+											<?php
+												for($i=1; $i<=12; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dtamins">
+											<option>mm</option>
+											<?php
+												for($i=1; $i<=59; $i++){
+											?>
+											<option><?php echo $i ?></option>
+											<?php } ?>
+										</select>
+									</div>
+									<div class="col-sm-2">
+										<select class="form-control" id="dtaTime">
+											<option>AM</option>
+											<option>PM</option>
+										</select>
+									</div>
+								</div>
+								</div>
+							</div>
+					<?php
+					}
+					?>
+
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 						<button type="submit" class="btn btn-primary">Assign</button>
@@ -108,24 +303,3 @@
 	  </div>
 	</div>
 </div> <!-- end Add Project Modal -->
-
-<script>
-	function enableNodeInput(){
-		document.getElementById("projectNodeName").disabled=false;
-		document.getElementById("projectCabinetNo").disabled=true;
-		document.getElementById("projectNodeName").placeholder="Enter Node Name";
-		document.getElementById("projectCabinetNo").placeholder="";
-	}
-	function enableCabinetInput(){
-		document.getElementById("projectNodeName").disabled=true;
-		document.getElementById("projectCabinetNo").disabled=false;
-		document.getElementById("projectNodeName").placeholder="";
-		document.getElementById("projectCabinetNo").placeholder="Enter Cabinet Number";
-	}
-	function disableInputs(){
-		document.getElementById("projectNodeName").disabled=true;
-		document.getElementById("projectCabinetNo").disabled=true;
-		document.getElementById("projectNodeName").placeholder="";
-		document.getElementById("projectCabinetNo").placeholder="";
-	}
-</script>
