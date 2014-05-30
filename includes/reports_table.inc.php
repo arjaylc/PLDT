bcdd025efc7b981e23924da44551a26e77f4cdf4
ramosjrz <jrzr94@gmail.com>
2014-05-30 14:06:31 +0800

    UPDATED
    reports in notification

--- includes/reports_table.inc.php
+++ includes/reports_table.inc.php
@@ -1,9 +1,11 @@
 <?php 
+        $idNo = $_SESSION['idNo'];
         require_once('includes/database_master.inc.php');
         $database_master = new DatabaseMaster();
         $query = "SELECT r.reportId, r.reportedBy, r.reportedTo, r.reportTitle, r.cabinetNo, r.nodeName, r.faultDescription, r.status, u.firstname, u.lastname
                   FROM reports AS r
-                  INNER JOIN users u ON r.reportedBy = u.idNo;
+                  INNER JOIN users u ON r.reportedBy = u.idNo
+                  WHERE r.reportedTo = $idNo;
                   ";
         $queryResult = $database_master->querySelect($query);
 
